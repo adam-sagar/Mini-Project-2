e1a307c0133d57856658286ac2d71aec06a80a88
adam-sagar <adamschrissagar@gmail.com>
2023-05-26 11:05:41 +1200

    Fixed search bar in place

--- Mini-Project-2/src/components/CardList.jsx
+++ Mini-Project-2/src/components/CardList.jsx
@@ -6,7 +6,6 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 
 function CardList() {
-
     const [data, setData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [currentCharacters, setCurrentCharacters] = useState([]);
@@ -16,7 +15,6 @@ function CardList() {
     const charactersPerPage = 8;
 
     useEffect(() => {
-
         axios
             .get(
                 "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
@@ -29,40 +27,31 @@ function CardList() {
     }, []);
 
     useEffect(() => {
-
         const filteredCharacters = data.filter((character) =>
             character.name.toLowerCase().startsWith(searchQuery.toLowerCase())
         );
 
         setMaxPage(Math.ceil(filteredCharacters.length / charactersPerPage));
-        setCurrentCharacters(
-            filteredCharacters.slice(0, charactersPerPage)
-        );
+        setCurrentCharacters(filteredCharacters.slice(0, charactersPerPage));
 
         setCurrentPage(1);
     }, [data, searchQuery]);
 
     const handleNextPage = () => {
-
         if (currentPage < maxPage) {
             const nextPage = currentPage + 1;
             const offset = charactersPerPage * (nextPage - 1);
             setCurrentPage(nextPage);
-            setCurrentCharacters(
-                data.slice(offset, offset + charactersPerPage)
-            );
+            setCurrentCharacters(data.slice(offset, offset + charactersPerPage));
         }
     };
 
     const handlePrevPage = () => {
-
         if (currentPage > 1) {
             const prevPage = currentPage - 1;
             const offset = charactersPerPage * (prevPage - 1);
             setCurrentPage(prevPage);
-            setCurrentCharacters(
-                data.slice(offset, offset + charactersPerPage)
-            );
+            setCurrentCharacters(data.slice(offset, offset + charactersPerPage));
         }
     };
 
@@ -70,17 +59,9 @@ function CardList() {
         setSearchQuery(event.target.value);
     };
 
-    const cardContainerStyle = {
-        // prevents character cards from overlapping when there is less than a full page of results
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: currentCharacters.length < charactersPerPage ? "space-around" : "flex-start",
-    };
-
-
     return (
         <div>
-            <Container>
+            <div className="search-bar">
                 <TextField
                     label="Search character..."
                     variant="outlined"
@@ -89,26 +70,26 @@ function CardList() {
                     fullWidth
                     sx={{ width: "200px", marginBottom: "30px" }}
                 />
-                <Grid container spacing={8} style={cardContainerStyle}>
-                    {currentCharacters.map((character) => (
-                        <Grid item xs={12} sm={6} md={4} lg={3} key={character.id}>
-                            <CharacterCard
-                                name={character.name}
-                                img={character.images.sm}
-                                publisher={character.biography.publisher}
-                                id={character.id}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
+            </div>
+            <Container className="card-list">
+                {currentCharacters.length === 0 ? (
+                    <p className="results">No results found.</p>
+                ) : (
+                    <Grid container spacing={8} className="card-container">
+                        {currentCharacters.map((character) => (
+                            <Grid item xs={12} sm={6} md={4} lg={3} key={character.id}>
+                                <CharacterCard
+                                    name={character.name}
+                                    img={character.images.sm}
+                                    publisher={character.biography.publisher}
+                                    id={character.id}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                )}
                 {currentCharacters.length === charactersPerPage && (
-                    <div
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            marginTop: "20px",
-                        }}
-                    >
+                    <div className="pagination">
                         <Button
                             variant="outline-dark"
                             onClick={handlePrevPage}
@@ -128,7 +109,6 @@ function CardList() {
             </Container>
         </div>
     );
-
 }
 
 export default CardList;
